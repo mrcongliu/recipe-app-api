0c50603d047ad8da13130b2c481d16eebdd994b9
mrcongliu <mrcongliu@gmail.com>
2024-01-13 12:01:24 +0800

    add serializer and create user api view

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -5,7 +5,7 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 
-from test_framework.test import APIClient
+from rest_framework.test import APIClient
 from rest_framework import status
 
 
